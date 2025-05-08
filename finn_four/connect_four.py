5dfc3984afe1d887bfcf4fee1e935111e5fc6d9a
Rachelle04 <rachellealex30@gmail.com>
2025-05-08 09:27:14 +0530

    Update connect_four.py

--- finn_four/connect_four.py
+++ finn_four/connect_four.py
@@ -37,15 +37,15 @@ def drop_piece(board, row, col, piece):
     board[row][col] = piece
 
 def is_valid_location(board, col):
-    return board[ROW_COUNT-1][col] == 0
+    return board[0][col] == 0
 
 def get_next_open_row(board, col):
-    for r in range(ROW_COUNT):
+    for r in range(ROW_COUNT-1, -1, -1):
         if board[r][col] == 0:
             return r
 
 def print_board(board):
-    print(np.flip(board, 0))
+    print(board)
 
 def winning_move(board, piece):
     # Check horizontal
